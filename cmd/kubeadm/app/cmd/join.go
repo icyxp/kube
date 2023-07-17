6e52017f9a3ea2c5f6da352808ff2322c4c60286
roc <xupeng@patsnap.com>
2023-07-17 18:23:37 +0800

    super kubeadm support 1.21.14

--- cmd/kubeadm/app/cmd/join.go
+++ cmd/kubeadm/app/cmd/join.go
@@ -42,6 +42,7 @@ import (
 	cmdutil "k8s.io/kubernetes/cmd/kubeadm/app/cmd/util"
 	kubeadmconstants "k8s.io/kubernetes/cmd/kubeadm/app/constants"
 	"k8s.io/kubernetes/cmd/kubeadm/app/discovery"
+	locallb "k8s.io/kubernetes/cmd/kubeadm/app/localLB"
 	configutil "k8s.io/kubernetes/cmd/kubeadm/app/util/config"
 	kubeconfigutil "k8s.io/kubernetes/cmd/kubeadm/app/util/kubeconfig"
 )
@@ -149,7 +150,8 @@ type joinData struct {
 
 // newCmdJoin returns "kubeadm join" command.
 // NB. joinOptions is exposed as parameter for allowing unit testing of
-//     the newJoinData method, that implements all the command options validation logic
+//
+//	the newJoinData method, that implements all the command options validation logic
 func newCmdJoin(out io.Writer, joinOptions *joinOptions) *cobra.Command {
 	if joinOptions == nil {
 		joinOptions = newJoinOptions()
@@ -168,6 +170,15 @@ func newCmdJoin(out io.Writer, joinOptions *joinOptions) *cobra.Command {
 			}
 
 			data := c.(*joinData)
+
+			if data.cfg.ControlPlane == nil {
+				fmt.Println("This is not a control plan")
+				if len(locallb.LVScare.Masters) != 0 {
+					locallb.CreateLocalLB()
+				}
+			} else {
+				fmt.Println("This is a control plan")
+			}
 
 			if err := joinRunner.Run(args); err != nil {
 				return err
@@ -190,6 +201,7 @@ func newCmdJoin(out io.Writer, joinOptions *joinOptions) *cobra.Command {
 				}
 
 			} else {
+				locallb.LVScareStaticPodToDisk("/etc/kubernetes/manifests")
 				// otherwise, if the node joined as a worker node;
 				// outputs the join done message and exit
 				fmt.Fprint(data.outputWriter, joinWorkerNodeDoneMsg)
@@ -276,6 +288,15 @@ func addJoinOtherFlags(flagSet *flag.FlagSet, joinOptions *joinOptions) {
 	flagSet.StringSliceVar(
 		&joinOptions.ignorePreflightErrors, options.IgnorePreflightErrors, joinOptions.ignorePreflightErrors,
 		"A list of checks whose errors will be shown as warnings. Example: 'IsPrivilegedUser,Swap'. Value 'all' ignores errors from all checks.",
+	)
+	flagSet.StringSliceVar(
+		&locallb.LVScare.Masters, "master", []string{}, "A list of ha masters, --master 192.168.1.2:6443  --master 192.168.1.3:6443  --master 192.168.1.4:6443",
+	)
+	flagSet.StringVar(
+		&locallb.LVScare.VIP, "vip", "169.254.169.100", "virtual ip",
+	)
+	flagSet.StringVar(
+		&locallb.LVScare.Image, "lvscare-image", "icyboy/lvscare:latest", "define lvscare image",
 	)
 	flagSet.StringVar(
 		&joinOptions.token, options.TokenStr, "",
